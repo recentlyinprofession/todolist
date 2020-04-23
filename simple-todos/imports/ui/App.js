360250ee122b9087b3db9b651e71fc34ccf71db5
Anastasia <56310790+recentlyinprofession@users.noreply.github.com>
2020-04-23 15:49:11 +0300

    10_change

--- simple-todos/imports/ui/App.js
+++ simple-todos/imports/ui/App.js
@@ -4,7 +4,6 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
-
 import Task from './Task.js';
 import AccountsUIWrapper from './AccountsUIWrapper.js';
 // App component - represents the whole app
@@ -23,12 +22,9 @@ class App extends Component {
     // Find the text field via the React ref
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-      owner: Meteor.userId(),           // _id of logged in user
-      username: Meteor.user().username,  // username of logged in user
-    });
+
+
+  Meteor.call('tasks.insert', text);
 
     // Clear form
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
@@ -42,13 +38,22 @@ class App extends Component {
     }
   renderTasks() {
     let filteredTasks = this.props.tasks;
-      if (this.state.hideCompleted) {
-        filteredTasks = filteredTasks.filter(task => !task.checked);
-      }
-      return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
-  }
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => {
+        const currentUserId = this.props.currentUser && this.props.currentUser._id;
+        const showPrivateButton = task.owner === currentUserId;
+
+        return (
+          <Task
+            key={task._id}
+            task={task}
+            showPrivateButton={showPrivateButton}
+          />
+        );
+      });
+    }
 
   render() {
     return (
@@ -65,29 +70,33 @@ class App extends Component {
            Hide Completed Tasks
          </label>
 
+
          <AccountsUIWrapper />
-         { this.props.currentUser ?
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-            <input
-              type="text"
-              ref="textInput"
-              placeholder="Type to add new tasks"
-            />
-          </form> : ''
-        }
+
+       { this.props.currentUser ?
+            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+              <input
+                type="text"
+                ref="textInput"
+                placeholder="Type to add new tasks"
+              />
+            </form> : ''
+          }
         </header>
 
         <ul>
           {this.renderTasks()}
-        </ul>
+        < /ul>
       </div>
     );
   }
 }
 export default withTracker(() => {
+  Meteor.subscribe('tasks');
+
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
-        currentUser: Meteor.user(),
+    currentUser: Meteor.user(),
   };
 })(App);
--- simple-todos/imports/ui/Task.js
+++ simple-todos/imports/ui/Task.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
 
 import { Tasks } from '../api/tasks.js';
 
@@ -6,19 +8,25 @@ import { Tasks } from '../api/tasks.js';
 export default class Task extends Component {
   toggleChecked() {
     // Set the checked property to the opposite of its current value
-    Tasks.update(this.props.task._id, {
-      $set: { checked: !this.props.task.checked },
-    });
+
+    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
   }
 
   deleteThisTask() {
-    Tasks.remove(this.props.task._id);
+  Meteor.call('tasks.remove', this.props.task._id);
   }
 
+  togglePrivate() {
+      Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
+    }
   render() {
     // Give tasks a different className when they are checked off,
     // so that we can style them nicely in CSS
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = classnames({
+     checked: this.props.task.checked,
+     private: this.props.task.private,
+   });
+  //  const taskClassName = this.props.task.checked ? 'checked' : '';
 
     return (
       <li className={taskClassName}>
@@ -32,6 +40,12 @@ export default class Task extends Component {
           checked={!!this.props.task.checked}
           onClick={this.toggleChecked.bind(this)}
         />
+
+        { this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+            { this.props.task.private ? 'Private' : 'Public' }
+          </button>
+        ) : ''}
 
         <span className="text">
            <strong>{this.props.task.username}</strong>: {this.props.task.text}
